8e002ee43c45dea6a8869464a4e9e625ffb6f472
chadlei <cblei@uci.edu>
2021-07-04 11:15:54 -0700

    add build command

--- configs/webpack/dev.js
+++ configs/webpack/dev.js
@@ -2,6 +2,9 @@
 const merge = require('webpack-merge')
 const webpack = require('webpack')
 const commonConfig = require('./common')
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(commonConfig, {
   mode: 'development',
@@ -10,12 +13,21 @@ module.exports = merge(commonConfig, {
     'webpack/hot/only-dev-server', // bundle the client for hot reloading, only- means to only hot reload for successful updates
     './index.tsx', // the entry point of our app
   ],
+  output: {
+    path: path.resolve(__dirname, 'build'), // change this
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
   devServer: {
     hot: true, // enable HMR on the server
+    contentBase: "./build",
   },
   devtool: 'cheap-module-eval-source-map',
   plugins: [
     new webpack.HotModuleReplacementPlugin(), // enable HMR globally
     new webpack.NamedModulesPlugin(), // prints more readable module names in the browser console on HMR updates
+    new HtmlWebpackPlugin({
+      template: path.resolve('./index.html'),
+    }),
   ],
 })
